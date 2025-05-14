9d85238d1f94ddb8b1cb0914980419c2a0e54502
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-05-14 07:17:50 +0530

    Update dl-fb.js

--- plugins/dl-fb.js
+++ plugins/dl-fb.js
@@ -31,7 +31,7 @@ cmd({
     const videoUrl = data.data.url;
     await conn.sendMessage(from, {
       video: { url: videoUrl },
-      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By JawadTechX ✅",
+      caption: "📥 *Facebook Video Downloaded*\n\n- Powered By 𝐅𝚫𝚰𝐒𝚫𝐋-𝚳𝐃 ✅",
     }, { quoted: m });
 
   } catch (error) {
