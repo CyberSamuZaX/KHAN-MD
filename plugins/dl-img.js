6d7e6a7fa96bee0d2e8d58a3d8d1f708449a472f
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-05-15 02:10:21 +0530

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 { 
                     image: { url: imageUrl },
-                    caption: `📷 Result for: ${query}\n> © Powered by JawadTechX`
+                    caption: `📷 Result for: ${query}\n> © Powered by 𝐅𝚫𝚰𝐒𝚫𝐋-𝚳𝐃`
                 },
                 { quoted: mek }
             );
