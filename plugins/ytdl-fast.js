dc11387e90b1952b68255fc3e53dafb0659ae754
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-05-15 18:44:14 +0530

    Update ytdl-fast.js

--- plugins/ytdl-fast.js
+++ plugins/ytdl-fast.js
@@ -6,7 +6,7 @@ const { ytsearch } = require('@dark-yasiya/yt-dl.js');
 
 cmd({ 
     pattern: "mp4", 
-    alias: ["video"], 
+    alias: ["video2"], 
     react: "🎥", 
     desc: "Download YouTube video", 
     category: "main", 
